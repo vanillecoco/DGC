6fdb297d534dd1036fdad3f6448577637bb6c69f
vanillecoco <vanillecoco.hs@gmail.com>
2021-06-28 20:15:51 -0700

    server js

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+import { great, PI, multiplyNumbers } from "./main2.js";
+
+alert(great);
+alert(PI);
+alert(multiplyNumbers(6,15));
+
+
+//npm install http-server -g
+//npx http-server
+//http://192.168.56.1:8081   
+//http://192.168.0.55:8081   
+//http://127.0.0.1:8081    
---
+++ JS/main2.js
@@ -0,0 +1,7 @@
+let great = "Hello World!";
+const PI = 3.14;
+
+function multiplyNumbers(a,b){
+    return a+b;
+}
+export {great, PI, multiplyNumbers};
